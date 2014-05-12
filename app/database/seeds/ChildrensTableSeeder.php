7922d7994617d74c2d8c22cd315303720afc459c
Chris-Reyna <christopher.reyna@yahoo.com>
2014-05-12 16:17:48 -0500

    initial commit of children's seeder

---
+++ app/database/seeds/ChildrensTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+class ChildrensTableSeeder extends Seeder {
+	public function run() {
+		DB::table('childrens')->delete();
+
+		$child = new Child();
+		$child->first_name = 'Julia';
+		$child->last_name = 'Reyna';
+		$child->gender = 'female';
+		$child->DOB = '03/29/00';
+		$child->child_id = 1;
+		$child->save();
+
+	}
+
+}
