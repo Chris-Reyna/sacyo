1a0dbeb1b79c5df989de640f0156c00f05380ccd
Chris-Reyna <christopher.reyna@yahoo.com>
2014-06-27 11:25:48 -0500

    cleaned up spaces

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -21,14 +21,14 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="{{ action('HomeController@showHome')}}">ST. ANN's CYO</a>
+      <a class="navbar-brand" href="{{action('HomeController@showHome')}}">ST. ANN's CYO</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="{{ action('HomeController@showHome')}}">Home</a></li>
-        <li><a href="{{ action('HomeController@showSignup')}}">Register with St.Ann's</a></li>
+        <li class="active"><a href="{{action('HomeController@showHome')}}">Home</a></li>
+        <li><a href="{{action('HomeController@showSignup')}}">Register with St.Ann's</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="http://ncaa.homestead.com/">West Zone Home Page</a></li>
