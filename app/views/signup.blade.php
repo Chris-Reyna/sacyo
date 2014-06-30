e7bdd1e759cd908fce6e6c932180f3e767d705df
Chris-Reyna <christopher.reyna@yahoo.com>
2014-06-30 15:14:24 -0500

    moved css and made changes to form

--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -5,98 +5,6 @@ body{
 
     background-image:url("img/BYbg.png");
 }
-
- .SUform{
-    color: #000;
-    background-color: #999;
-    
-    -webkit-box-shadow: 0 8px 75px -6px black;
-    -moz-box-shadow: 0 8px 75px -6px black;
-    box-shadow: 0 8px 75px -6px black;
-    padding: 50px;
- }
-a:hover.btn.btn-primary, .btn.btn-primary {
-    color: #fff;
-    background-color: #000;
-    border-color: #999;
-
-}
-.formtext{
-    float: right;
-}
-
-input{
-   border-style: solid; 
-}
-/*Whodat pic animation*/
-.animated { 
-    -webkit-animation-duration: 1s; 
-    animation-duration: 1s; 
-    -webkit-animation-fill-mode: both; 
-    animation-fill-mode: both; 
-} 
-
-@-webkit-keyframes rollIn { 
-    0% { opacity: 0; -webkit-transform: translateX(-100%) rotate(-120deg); } 
-    100% { opacity: 1; -webkit-transform: translateX(0px) rotate(0deg); } 
-} 
-@keyframes rollIn { 
-    0% { opacity: 0; transform: translateX(-100%) rotate(-120deg); } 
-    100% { opacity: 1; transform: translateX(0px) rotate(0deg); } 
-} 
-.rollIn { 
-    -webkit-animation-name: rollIn; 
-    animation-name: rollIn; 
-}
-/*end whodat*/
-/*form animation*/
-
-.animated { 
-    -webkit-animation-duration: 1s; 
-    animation-duration: 1s; 
-    -webkit-animation-fill-mode: both; 
-    animation-fill-mode: both; 
-} 
-
-@-webkit-keyframes bounceInRight { 
-    0% { 
-        opacity: 0; 
-        -webkit-transform: translateX(2000px); 
-    } 
-    60% { 
-        opacity: 1; 
-        -webkit-transform: translateX(-30px); 
-    } 
-    80% { 
-        -webkit-transform: translateX(10px); 
-    } 
-    100% { 
-        -webkit-transform: translateX(0); 
-    } 
-} 
-
-@keyframes bounceInRight { 
-    0% { 
-        opacity: 0; 
-        transform: translateX(2000px); 
-    } 
-    60% { 
-        opacity: 1; 
-        transform: translateX(-30px); 
-    } 
-    80% { 
-        transform: translateX(10px); 
-    } 
-    100% { 
-        transform: translateX(0); 
-    } 
-} 
-
-.bounceInRight { 
-    -webkit-animation-name: bounceInRight; 
-    animation-name: bounceInRight; 
-}
-/*fornm end*/
 </style>
 @stop
 
@@ -107,8 +15,6 @@ input{
     <div class="container">
         <div class="span3">
             <div>
-                
-
                 <form action= "UsersController@store" method= "GET"class= "SUform" >
                     <div class="row">
                     <div class="col-md-6">
@@ -117,43 +23,43 @@ input{
                         <img id="animated-example" class="animated rollIn" src="img/who.jpg" alt="Small pic">
                     </div>
                     <div id="animated-example" class="animated bounceInRight">
-                    <div class="col-md-6">
-                        <div>
-                            <label>First Name</label>
-                            <div><input type="text" name="firstname" class="span3"></div>
-                            
-                        </div>
-                        <div>
-                            <label>Last Name</label>
-                            <div><input type="text" name="lastname" class="span3"></div>
-                        </div>
-                        <div>
-                            <label>Relationship to Player(s)</label>
-                            <div><input type="text" name="relationship" class="span3"></div>
-                        </div>
-                        <div>
-                            <label>Email </label>
-                            <div><input type="email" name="email" class="span3"></div>
-                        </div>
-                        <div>
-                            <label>Address</label>
-                            <div><input type="text" name="address" class="span3"></div>
-                        </div>
-                        <div>
-                            <label>Password</label>
-                            <div><input type="password" name="password" class="span3"></div>
-                        </div>
-                        <div>
-                            <label>Phone Number </label>
-                            <div><input type="text" name="phone" class ="span3"></div>
-                        </div>
-                        <br>
-                        <div class = "container">
+                        <div class="col-md-6">
                             <div>
-                                <input type="submit" value="Sign up" class="btn btn-primary ">  
+                                <label>First Name</label>
+                                <div><input type="text" name="firstname" class="span3"></div>
+                                
+                            </div>
+                            <div>
+                                <label>Last Name</label>
+                                <div><input type="text" name="lastname" class="span3"></div>
+                            </div>
+                            <div>
+                                <label>Relationship to Player(s)</label>
+                                <div><input type="text" name="relationship" class="span3"></div>
+                            </div>
+                            <div>
+                                <label>Email </label>
+                                <div><input type="email" name="email" class="span3"></div>
+                            </div>
+                            <div>
+                                <label>Address</label>
+                                <div><input type="text" name="address" class="span3"></div>
+                            </div>
+                            <div>
+                                <label>Password</label>
+                                <div><input type="password" name="password" class="span3"></div>
+                            </div>
+                            <div>
+                                <label>Phone Number </label>
+                                <div><input type="text" name="phone" class ="span3"></div>
+                            </div>
+                            <br>
+                            <div>
+                                <div>
+                                    <input type="submit" value="Sign up" class="btn btn-primary ">  
+                                </div>
                             </div>
                         </div>
-                    </div>
                     </div>
                     <br>
                 </form>
