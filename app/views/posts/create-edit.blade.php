c08ec8cd747d819caa4cc82e031c100a17ca9147
Chris-Reyna <christopher.reyna@yahoo.com>
2014-07-01 15:59:38 -0500

    rename create bade to creat-edit

--- app/views/posts/create-edit.blade.php
+++ app/views/posts/create-edit.blade.php
@@ -19,25 +19,41 @@
     -moz-border-radius: 15px;
 	border-radius: 25px;
  }
+ 
 </style>
 @stop
 @section('content')
-<h1>Create Message for Message Board</h1>
+
+@if (isset($post))
+<h1>Edit Message</h1>
+
+@else
+<h1>Create Message for Message Board</h1>	
+@endif
 <div class="container ">
 	<div class="row "id="Mform" >
 		<div class="col-md-6">
-			<form id="creator" action="{{{ action('PostsController@store') }}}" method="POST">
+			@if (isset($post))
+			{{ Form::open(array('action' => array('PostsController@update', $post->id), 'method' => 'PUT')) }}
+			@else
+			{{ Form::open(array('action' => 'PostsController@store')) }}
+			@endif
 				<div>
-					<label for="Title">Title</label><br>
-					<input type="text" id="Title" name="Title" value="{{{ Input::old('Title')}}}">
+					{{ $errors->first('title', '<span class= "help">:message</span>')}}<br>
+					{{ Form::label('title', 'Title') }}
+					<br>
+					{{ Form::text('title') }}
 				</div>
 				<br>
 				<div>
-					<label for="message">Message</label><br>
-					<textarea id="message" name="message" rows="15" cols="70" >{{{ Input::old('Body')}}}</textarea>
+					{{ Form::label('message', 'Message') }}
+					<br>
+					{{ Form::textarea('message', null, ['size' => '70x15','id' => 'message']) }}
+					
 				</div>
 				<button type="submit">Create New Post</button>
-			</form>
+			{{ Form::close() }}
+
 		</div>
 		<div class="col-md-6">
 			<div id="animated-example" class="animated bounceInRight">
