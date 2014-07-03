0bdae5e0e01116a4436f9b221289e9455c8d373a
Chris-Reyna <christopher.reyna@yahoo.com>
2014-07-03 16:10:22 -0500

    set time zone

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -8,6 +8,11 @@ class BaseModel extends Eloquent{
 
 	/**
 	*Setting an accessor to change utc to cst using carbon library */
+    public function getCreatedAtAttribute($value)
+	{
+	    $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+	    return $utc->setTimezone('America/Chicago');
+	}
 	public function getUpdatedAtAttribute($value)
 	{
 	    $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
