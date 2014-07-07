8dcbe6befa3b72815abbd0010fa98ceb658aaedc
Chris-Reyna <christopher.reyna@yahoo.com>
2014-07-07 14:26:49 -0500

    fixed login deleted remember me box

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,13 +4,9 @@
 @section('content')
 <div class="container" id="login">
 	{{ Form::open(array('action' => 'HomeController@doLogin', 'class' => 'form-signin')) }}
-	<form class="form-signin" role="form">
 	    <h2 class="form-signin-heading">Administrative Login</h2>
-	    <input name="email" class="form-control" placeholder="Email address" required autofocus>
+	    <input name="email" type="text"class="form-control" placeholder="Email Address" value="{{{ Input::old('email')}}}" required autofocus>
 	    <input name="password" type="password" class="form-control" placeholder="Password" required>
-	    <label class="checkbox">
-	      <input type="checkbox" value="remember-me"> Remember me</input>
-	    </label>
 	    <button class="btn btn-lg btn-primary active" type="submit">Login</button>
 </div>
 
