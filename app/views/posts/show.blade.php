e21dfad5a0ae7ed9ccf338b3460fa47aa7a3f9f8
Chris-Reyna <christopher.reyna@yahoo.com>
2014-07-07 11:48:51 -0500

    added delete to show

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -21,12 +21,44 @@
 </div>
 	<hr>
 	<div>
+		{{Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'delete', 'id' => 'formDeletePost'))}}
+		{{ Form::submit('Delete') }}
+		{{Form::close()}}|
 		
-		<a class='links' href="#" id="btnDeletePost">Delete Post</a> |
-	
 		<a class='links' href="{{{ action('PostsController@edit', $post->id)}}}">Edit Post</a> |
 		
 		<a class='links' href="{{{ action('PostsController@index')}}}">Return to Message Listing</a>
 		
-	</div>			
+	</div>	
+			
+@stop
+@section('bottom-script')
+
+<script>
+	$('#btnDeletePost').on('click', function (e){
+		e.preventDefault();
+		if(confirm('Are you sure you want to delete this post?')){
+			$('#formDeletePost').submit();
+		}
+	});
+</script>
+<hr>
+<div>
+	    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'reynablogdev'; // required: replace example with your forum shortname
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+    
+</div>
+
 @stop
